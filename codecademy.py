6fdd0106f63830c42b21381bc2ce8878bccedbd0
tecx <tecx.gno@gmail.com>
2018-07-18 19:43:37 +0800

    Completed online tutorial

---
+++ codecademy.py
@@ -0,0 +1,102 @@
+#=========================================
+# Example for filter() and lambda
+garbled = "IXXX aXXmX aXXXnXoXXXXXtXhXeXXXXrX sXXXXeXcXXXrXeXt mXXeXsXXXsXaXXXXXXgXeX!XX"
+
+message = filter(lambda k: k != "X", garbled)
+print message
+
+#=========================================
+# BITwise
+print 5 >> 4  # Right Shift
+print 5 << 1  # Left Shift
+print 8 & 5   # Bitwise AND
+print 9 | 4   # Bitwise OR
+print 12 ^ 42 # Bitwise XOR
+print ~88     # Bitwise NOT
+
+print 0b1,    #1
+print 0b10,   #2
+print 0b11,   #3
+print 0b100,  #4
+print 0b101,  #5
+print 0b110,  #6
+print 0b111   #7
+print "******"
+print 0b1 + 0b11
+print 0b11 * 0b11
+
+print int("1",2)
+print int("10",2)
+print int("111",2)
+print int("0b100",2)
+print int(bin(5),2)
+# Print out the decimal equivalent of the binary 11001001.
+print int("11001001", 2)
+
+# Flip the n-th Bit
+def flip_bit(number, n):
+  mask = (0b1 << (n-1))
+  result = number ^ mask
+  return bin(result)
+
+
+#=========================================
+# Class
+class Fruit(object):
+  """A class that makes various tasty fruits."""
+  def __init__(self, name, color, flavor, poisonous):
+    self.name = name
+    self.color = color
+    self.flavor = flavor
+    self.poisonous = poisonous
+  def description(self):
+    print "I'm a %s %s and I taste %s." % (self.color, self.name, self.flavor)
+  def is_edible(self):
+    if not self.poisonous:
+      print "Yep! I'm edible."
+    else:
+      print "Don't eat me! I am super poisonous."
+
+lemon = Fruit("lemon", "yellow", "sour", False)
+
+lemon.description()
+lemon.is_edible()
+
+
+# Simple class
+class Animal(object):
+  def __init__(self, name):
+    self.name = name
+
+zebra = Animal("Jeffrey")
+print zebra.name
+
+
+# Present object in specific format
+class Point3D(object):
+  def __init__(self, x, y, z):
+    self.x = x
+    self.y = y
+    self.z = z
+    
+  def __repr__(self):
+    return "(%d, %d, %d)" % (self.x, self.y, self.z)
+  
+my_point = Point3D(1, 2, 3)
+
+print my_point
+
+
+#=============================================
+# File I/O
+
+# Example 1
+my_list = [i ** 2 for i in range(1, 11)]
+# Generates a list of squares of the numbers 1 - 10
+
+f = open("output.txt", "w")
+
+for item in my_list:
+  f.write(str(item) + "\n")
+
+f.close()
